143be8587335e2175c51cda0ee10983b34c6298b
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-07-08 16:10:47 +0100

    Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import Homepage from './Homepage';
+// import Portfolio from './Portfolio';
+// import Contact from './Contact';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <Link to='/'>
+            <button><img src="../public/ixesta.png" alt="Home" /></button>
+          </Link>
+        </div>
+        {/* <Route exact path="/" component={Homepage} /> */}
+        {/* <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} /> */}
+      </Router>
+    )
   }
 }
 
