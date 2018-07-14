474628de090ea60823ca620913ce4c4c0040c3ea
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-07-14 15:09:32 +0100

    smaller cards

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,17 +7,15 @@ class Portfolio extends Component {
 
         <div class="card">
           <div class="card-image">
-            <img class="activator" src="/Tripster/logo2.png" alt="" width='40%' />
+            <img src="/Tripster/logo2.png" alt="" width='40%' />
             {/* <video width="320" height="240" controls>
               <source src="/tripster.mp4" type="video/mp4" />>
             </video> */}
           </div>
           <div class="card-content">
             <p class="card-name">Tripster</p>
-            <p class="card-description">Your travel app</p>
+            {/* <p class="card-description">Your travel app</p> */}
 
-          </div>
-          <div class="card-reveal">
             <p><a class="card-text" href="/tripster.mp4" target="_blank">Video</a></p>
             <p><a class="card-text" href="https://github.com/ixesta/Tripster" target="_blank">GitHub repo</a></p>
 
@@ -26,15 +24,14 @@ class Portfolio extends Component {
 
 
         <div class="card">
+
           <div class="card-image">
-            <img class="activator" src="/typewriter2.png" alt="" width='40%' />
+            <img src="/typewriter2.png" alt="" width='40%' />
           </div>
+
           <div class="card-content">
             <p class="card-name">NC News</p>
-            <p class="card-description">Social News Website</p>
-
-          </div>
-          <div class="card-reveal">
+            {/* <p class="card-description">Social News Website</p> */}
 
             <p ><a class="card-text" href="https://ro-nc-app.herokuapp.com/" target="_blank">Deployed version</a></p>
             <p><a class="card-text" href="https://github.com/ixesta/NC-News-FE" target="_blank">GitHub repo</a></p>
@@ -42,27 +39,6 @@ class Portfolio extends Component {
           </div>
         </div>
 
-
-        {/* <a href="https://github.com/ixesta/Tripster">
-          <div class='card'>
-            <img src="/Tripster/logo.png" alt="" width='40%' />
-            <div class="container">
-              <h2><b>Tripster</b></h2>
-              <p>Travel App</p>
-            </div>
-          </div>
-        </a>
-
-
-        <a href="https://ro-nc-app.herokuapp.com/">
-          <div class='card'>
-            <img src="/typewriter.png" alt="" width='40%' />
-            <div class="card-container">
-              <h2><b>Northcoders News</b></h2>
-              <p>Social News</p>
-            </div>
-          </div>
-        </a> */}
 
       </div>
     )
