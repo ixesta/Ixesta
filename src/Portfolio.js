3e7402b7406095eddfb77b8482141d2548ce77a8
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-07-15 20:57:24 +0100

    icons to cards

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,17 +7,23 @@ class Portfolio extends Component {
 
         <div class="card">
           <div>
-            {/* <img class="card-image" src="/Tripster/logo2.png" alt="" width='40%' /> */}
-            {/* <video width="320" height="240" controls>
-              <source src="/tripster.mp4" type="video/mp4" />>
-            </video> */}
+
           </div>
           <div class="card-content">
             <p class="card-name">Tripster</p>
-            {/* <p class="card-description">Your travel app</p> */}
 
-            <p><a class="card-text" href="/tripster.mp4" target="_blank">Video</a></p>
-            <p><a class="card-text" href="https://github.com/ixesta/Tripster" target="_blank">GitHub repo</a></p>
+
+            <p>
+              <a href="/tripster.mp4" target="_blank">
+                <img class="video-icon" src="./video.png" alt="Promotional video" />
+              </a>
+            </p>
+
+            <p>
+              <a href="https://github.com/ixesta/Tripster" target="_blank" rel="noopener noreferrer">
+                <img class="video-icon" src="./code.png" alt="GitHub" />
+              </a>
+            </p>
 
           </div>
         </div>
@@ -25,16 +31,21 @@ class Portfolio extends Component {
 
         <div class="card">
 
-          {/* <div>
-            <img class="card-image" src="/typewriter2.png" alt="" width='40%' />
-          </div> */}
+
 
           <div class="card-content">
             <p class="card-name">NC News</p>
-            {/* <p class="card-description">Social News Website</p> */}
 
-            <p ><a class="card-text" href="https://ro-nc-app.herokuapp.com/" target="_blank">Deployed version</a></p>
-            <p><a class="card-text" href="https://github.com/ixesta/NC-News-FE" target="_blank">GitHub repo</a></p>
+            <p >
+              <a class="card-text" href="https://ro-nc-app.herokuapp.com/" target="_blank">
+                <img class="video-icon" src="./http.png" alt="Web" />
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/ixesta/NC-News-FE" target="_blank">
+                <img class="video-icon" src="./code.png" alt="GitHub" />
+              </a>
+            </p>
 
           </div>
         </div>
